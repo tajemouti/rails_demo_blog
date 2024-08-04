234bb3c7ea46376b0b7f740b6cef782c11b212bc
tajemouti <tajemouti@gmail.com>
2024-08-04 16:01:10 +0100

    Add submitted action to CommentsMailer to send notification email for new comments

--- app/mailers/comments_mailer.rb
+++ app/mailers/comments_mailer.rb
@@ -5,9 +5,9 @@ class CommentsMailer < ApplicationMailer
   #
   #   en.comments_mailer.submitted.subject
   #
-  def submitted
-    @greeting = "Hi"
+  def submitted(comment)
+    @comment = comment
 
-    mail to: "to@example.org"
+    mail to: "tajemouti@gmail.com", subject: "New comment!"
   end
 end
